ecafb89699050a6341740020a911d2a72c62c562
azjustin1 <48974108+azjustin1@users.noreply.github.com>
2024-03-20 09:46:52 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ call .\install.bat >nul
 echo Install completed
 call .\build.bat >nul
 echo Build completed
-call .\run-server.bat
+npm run server
 
 echo The web is running at http://localhost:3000
